bf0f314da793977b5a0a4c290da02d1d073e457c
unknown <jordispir@gmail.com>
2020-12-22 12:28:23 +0100

    reaparición de tuberias random

--- main.py
+++ main.py
@@ -3,80 +3,11 @@ import random
 
 pygame.init()
 pygame.font.init()
-<<<<<<< HEAD
-
-anchura, altura = 1200, 800
-window = pygame.display.set_mode((anchura, altura))
-out = False
-
-clock = pygame.time.Clock()
-clock.tick(30)
-
-xFlapy, yFlapy = 100, (altura/2)
-velocity = 1
-
-
-tuberias = pygame.sprite.Group()
-flapyBird = None
-backGround = None
-xTuberia = random.randrange(100, anchura-100)
-
-
-vidasText = None
-xVidas, yVidas = 800, 100
-
-vidas = 3
-
-
-def loadObstaculos():
-    tuberiaUp = pygame.image.load("tuberiaUp.png")
-    tuberiaUpSprite = pygame.sprite.Sprite()
-    tuberiaUpSprite.image = tuberiaUp
-    tuberiaUpSprite.rect = tuberiaUp.get_rect()
-    tuberiaUpSprite.rect.x = random.randrange(100, anchura-100)
-    tuberiaUpSprite.rect.y = altura - tuberiaUp.get_height()
-
-    tuberiaDown = pygame.image.load("tuberiaDown.png")
-    tuberiaDownSprite = pygame.sprite.Sprite()
-    tuberiaDownSprite.image = tuberiaDown
-    tuberiaDownSprite.rect = tuberiaDown.get_rect()
-    tuberiaDownSprite.rect.x = random.randrange(100, anchura-100)
-    tuberiaDownSprite.rect.y = 0
-
-    tuberias.add(tuberiaUpSprite)
-    tuberias.add(tuberiaDownSprite)
-
-
-def loadFlapyBird():
-    global flapyBird
-    flapyBirdImage = pygame.image.load("flapyBird.png")
-    flapyBird = pygame.sprite.Sprite()
-    flapyBird.image = flapyBirdImage
-    flapyBird.rect = flapyBirdImage.get_rect()
-    flapyBird.rect.x = xFlapy
-    flapyBird.rect.y = yFlapy
-
-def loadBackGround():
-    global backGround
-    backGround = pygame.image.load("bg.jpg")
-
-def statusVida():
-    global vidasText 
-    myfont = pygame.font.SysFont('Comic Sans MS', 30)
-    vidasText = myfont.render("vidas:" + str(vidas), False, (0, 0, 0))
-
-def dibuja():
-    window.blit(backGround, (0, 0))
-    window.blit(vidasText, (xVidas, yVidas))
-    tuberias.draw(window)
-    window.blit(flapyBird.image, (flapyBird.rect.x, flapyBird.rect.y))
-
-=======
 
 fps = pygame.time.Clock()
 fps.tick(60)
 
-anchura, altura = 1800, 900 
+anchura, altura = 1200, 800 
 window = pygame.display.set_mode((anchura, altura))
 
 tuberias = pygame.sprite.Group()
@@ -100,7 +31,6 @@ yVidas = 50
 velocity = 1
 
 vidas = 3
->>>>>>> 983b66a8d624ddc1d737fb39b4d7b062a902df13
 
 def tuberiaUp_inicial():
     global tuberiaUp
@@ -131,7 +61,7 @@ def load_sprites():
     global tuberiaUpSprite
 
     tuberiaUpSprite = pygame.sprite.Sprite()
-    tuberiaUpSprite.image = tuberiaUp
+    tuberiaUpSprite.image = tuberiaUp #TODO refresh image
     tuberiaUpSprite.rect = tuberiaUp.get_rect()
     tuberiaUpSprite.rect.x = xTuberiaUp 
     tuberiaUpSprite.rect.y = yTuberiaUp
@@ -142,8 +72,8 @@ def load_sprites():
     tuberiaDownSprite.rect.x = xTuberiaDown
     tuberiaDownSprite.rect.y = yTuberiaDown
 
-    tuberias.add(tuberiaUpSprite)
     tuberias.add(tuberiaDownSprite)
+    tuberias.add(tuberiaUpSprite)
 
 
 def load_flapyBird():
@@ -180,28 +110,18 @@ def update_x():
         tuberiaDownSprite.rect.x = anchura  
         outDisplay1 = True
     elif tuberiaUpSprite.rect.x < 0:
+        tuberiaUpSprite.rect.x = anchura
         outDisplay2 = True
 
     else:
         outDisplay1, outDisplay2 = False, False
 
 def update_y():
-    global tuberiaUpSprite
     if outDisplay2:
+        tuberiaUp_inicial()
 
-        tuberia = random.choice(imageListUp)
-        tuberiaNew = pygame.image.load("tuberiaUp/"+ tuberia)
-        tuberiaNewHeight = tuberiaNew.get_height()
 
-        tuberiaNewSprite = pygame.sprite.Sprite()
-        tuberiaNewSprite.image = tuberiaNew
-        tuberiaNewSprite.rect = tuberiaNew.get_rect()
-        tuberiaNewSprite.rect.x = anchura 
-        tuberiaNewSprite.rect.y = altura - tuberiaNewHeight 
-
-        tuberias.add(tuberiaNewSprite)
-
-        tuberiaNewSprite.rect.x -= 4
+#CONTINUE
 
 
 def move_flapyBird():
@@ -214,18 +134,6 @@ def move_flapyBird():
 
     if key[pygame.K_SPACE]:
         yFlapy -= (velocity/2 * 10)
-<<<<<<< HEAD
-
-    flapyBird.rect.x = xFlapy
-    flapyBird.rect.y = yFlapy
-
-def colision():
-    global vidas
-    if (pygame.sprite.spritecollideany(flapyBird, tuberias)):
-        vidas -= 1
-
-
-=======
 
     flapyBird.rect.x = xFlapy
     flapyBird.rect.y = yFlapy
@@ -251,39 +159,15 @@ def colision():
 
 
 
->>>>>>> 983b66a8d624ddc1d737fb39b4d7b062a902df13
 def endGame():
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             return True
-<<<<<<< HEAD
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                return True
-
-
-
-    if vidas == 0:
-        return True
-
-    return False
-
-def update():
-    window.scroll(int(-xFlapy), int(-yFlapy))
-
-loadFlapyBird()
-loadObstaculos()
-loadBackGround()
-while not endGame():
-    window.fill((0, 255, 255))
-
-    moveFlapyBird()
-    colision()
-
-    statusVida()
-    dibuja()
-=======
+                return True 
 
 
     return False
@@ -302,6 +186,5 @@ while not endGame():
     dibuja()
     update_x()
     update_y()
->>>>>>> 983b66a8d624ddc1d737fb39b4d7b062a902df13
     pygame.display.flip()
 
